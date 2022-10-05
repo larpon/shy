92c8cd4375fcc8487d7b92c6d078b28ec2aaf31b
lmp <dev.larpon@gmail.com>
2022-10-05 12:46:39 +0200

    examples: fixup sound example

--- examples/sound/sound.v
+++ examples/sound/sound.v
@@ -21,10 +21,10 @@ mut:
 
 [markused]
 pub fn (mut a App) init() ! {
-	a.ExampleApp.init()! // Important to let the embed initialize before us
+	a.ExampleApp.init()!
 
 	a.sound = a.easy.new_sound(
-		path: a.ExampleApp.asset('sfx/shy_sound_01.wav')
+		path: a.ExampleApp.asset('sfx/shy.wav')
 		// Allows for repeating the sound on top of itself `max_repeats` amount of times.
 		// NOTE consumes memory for each repeat instance.
 		max_repeats: 4
@@ -36,13 +36,13 @@ pub fn (mut a App) frame(dt f64) {
 	is_looping_str := if a.sound.is_looping() { 'looping' } else { 'not looping' }
 	is_playing_str := if a.sound.is_playing() { 'playing' } else { 'not playing' }
 	a.quick.text(
-		text: 'Press a key or click in window to play a sound\nPress "L" to loop (currently $is_looping_str)\nSound is $is_playing_str'
+		text: 'Press a key, touch or click in window to play a sound\nPress "L" to loop (currently $is_looping_str)\nSound is $is_playing_str'
 	)
 }
 
 [markused]
 pub fn (mut a App) event(e shy.Event) {
-	a.ExampleApp.event(e) // Let the embed handle it's events before us
+	a.ExampleApp.event(e)
 
 	match e {
 		shy.KeyEvent {
