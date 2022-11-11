f5e92a96c99f5f926494690efddf6e7ab61bbf85
lmp <dev.larpon@gmail.com>
2022-11-11 17:40:27 +0100

    api: partially fix usage with `-live`

--- lib/shy.api.v
+++ lib/shy.api.v
@@ -6,11 +6,11 @@
 module lib
 
 import shy.vec
-import shy.mth
+// TODO BUG see consts below // import shy.mth
 
 pub const (
-	deg2rad = mth.deg2rad
-	rad2deg = mth.rad2deg
+	deg2rad = f32(0.017453292519943295) // TODO BUG with `-live` mth.deg2rad
+	rad2deg = f32(57.29577951308232) // TODO BUG with `-live` mth.rad2deg
 )
 
 [inline]
