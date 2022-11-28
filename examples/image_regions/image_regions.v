96eafe0bbd15824d4ca8cd72d5b1e23042e29e1c
lmp <dev.larpon@gmail.com>
2022-11-28 15:37:55 +0100

    examples: add image region example

---
+++ examples/image_regions/image_regions.v
@@ -0,0 +1,69 @@
+// Copyright(C) 2022 Lars Pontoppidan. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
+module main
+
+import shy.lib as shy
+import shy.embed
+
+fn main() {
+	mut app := &App{}
+	shy.run[App](mut app)!
+}
+
+[heap]
+struct App {
+	embed.ExampleApp
+}
+
+[markused]
+pub fn (mut a App) init() ! {
+	a.ExampleApp.init()!
+
+	a.quick.load(
+		uri: a.asset('image/shy_logo.png')
+	)!
+}
+
+[markused]
+pub fn (mut a App) frame(dt f64) {
+	cx := shy.half * a.window.width()
+	cy := shy.half * a.window.height()
+
+	margin := f32(10)
+
+	a.quick.image(
+		x: cx
+		y: cy
+		uri: a.asset('image/shy_logo.png')
+		origin: .bottom_right
+		offset: shy.vec2(-margin, -margin)
+		region: shy.Rect{0, 0, 256, 256}
+	)
+	a.quick.image(
+		x: cx
+		y: cy
+		uri: a.asset('image/shy_logo.png')
+		origin: .bottom_left
+		offset: shy.vec2(margin, -margin)
+		region: shy.Rect{256, 0, 256, 256}
+	)
+
+	a.quick.image(
+		x: cx
+		y: cy
+		uri: a.asset('image/shy_logo.png')
+		origin: .top_left
+		offset: shy.vec2(margin, margin)
+		region: shy.Rect{256, 256, 256, 256}
+	)
+
+	a.quick.image(
+		x: cx
+		y: cy
+		uri: a.asset('image/shy_logo.png')
+		origin: .top_right
+		offset: shy.vec2(-margin, margin)
+		region: shy.Rect{0, 256, 256, 256}
+	)
+}
