8e6b74bb872f948dc00f0c524acceb4ff4d2742c
lmp <dev.larpon@gmail.com>
2022-12-07 10:42:06 +0100

    math: add Greatest Common Divisor functions

--- mth/math.v
+++ mth/math.v
@@ -1,5 +1,6 @@
 module mth
 
+import math // TODO
 // TODO v module BUG prevents calling this "math"?!, also consider "manmade math"
 
 pub const (
@@ -8,20 +9,31 @@ pub const (
 	rad2deg  = f32(360 / (pi * 2))
 )
 
-// min returns the minimum of `a` and `b`
+// min returns the minimum of `a` and `b`.
 [inline]
 pub fn min[T](a T, b T) T {
 	return if a < b { a } else { b }
 }
 
-// max returns the maximum of `a` and `b`
+// max returns the maximum of `a` and `b`.
 [inline]
 pub fn max[T](a T, b T) T {
 	return if a > b { a } else { b }
 }
 
-// abs returns the absolute value of `a`
+// abs returns the absolute value of `a`.
 [inline]
 pub fn abs[T](a T) T {
 	return if a < 0 { -a } else { a }
+}
+
+// gcd finds the Greatest Common Divisor between two numbers.
+pub fn gcd[T](a T, b T) T {
+    return T(if b == 0 { a } else { gcd(b, T(math.mod(f64(a),f64(b)))) })
+}
+
+// reduce reduces a fraction by finding the Greatest Common Divisor and dividing by it.
+pub fn reduce[T](numerator T, denominator T) (T,T) {
+	g_c_d := gcd(numerator,denominator)
+  	return numerator/g_c_d, denominator/g_c_d
 }
