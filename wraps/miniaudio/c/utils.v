826bdd37acc7bb07a36c9d675712b59a7ac34797
lmp <dev.larpon@gmail.com>
2023-01-02 14:48:31 +0100

    all: use original copyright year

--- wraps/miniaudio/c/utils.v
+++ wraps/miniaudio/c/utils.v
@@ -1,4 +1,4 @@
-// Copyright(C) 2019-2021 Lars Pontoppidan. All rights reserved.
+// Copyright(C) 2019 Lars Pontoppidan. All rights reserved.
 // Use of this source code is governed by an MIT license file distributed with this software package
 // miniaudio (https://github.com/dr-soft/miniaudio)
 // is licensed under the unlicense and, are thus, in the publiic domain.
