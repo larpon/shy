10ba658411091cd73206d59438a5d40e9a339e56
lmp <dev.larpon@gmail.com>
2023-02-01 13:27:00 +0100

    analyse: add min/max functions

--- analyse/analyse.v
+++ analyse/analyse.v
@@ -25,10 +25,94 @@ pub fn count[T](key string, amount T) {
 	mut a := unsafe { analyse.hack } // TODO
 	$if T is int {
 		a.entries[key] = '${a.entries[key].int() + amount}'
+	} $else $if T is u32 {
+		a.entries[key] = '${a.entries[key].u32() + amount}'
+	} $else $if T is i64 {
+		a.entries[key] = '${a.entries[key].i64() + amount}'
+	} $else $if T is u64 {
+		a.entries[key] = '${a.entries[key].u64() + amount}'
 	} $else $if T is f32 {
 		a.entries[key] = '${a.entries[key].f32() + amount}'
 	} $else $if T is f64 {
 		a.entries[key] = '${a.entries[key].f64() + amount}'
+	} $else {
+		t := T{}
+		panic('${@STRUCT}.${@FN}: ${typeof(t).name} is not supported')
+	}
+}
+
+[if shy_analyse ?]
+pub fn max[T](key string, amount T) {
+	mut a := unsafe { analyse.hack } // TODO
+	$if T is int {
+		cur_amount := a.entries[key].int()
+		if amount > cur_amount {
+			a.entries[key] = '${amount}'
+		}
+	} $else $if T is u32 {
+		cur_amount := a.entries[key].u32()
+		if amount > cur_amount {
+			a.entries[key] = '${amount}'
+		}
+	} $else $if T is i64 {
+		cur_amount := a.entries[key].i64()
+		if amount > cur_amount {
+			a.entries[key] = '${amount}'
+		}
+	} $else $if T is u64 {
+		cur_amount := a.entries[key].u64()
+		if amount > cur_amount {
+			a.entries[key] = '${amount}'
+		}
+	} $else $if T is f32 {
+		cur_amount := a.entries[key].f32()
+		if amount > cur_amount {
+			a.entries[key] = '${amount}'
+		}
+	} $else $if T is f64 {
+		cur_amount := a.entries[key].f64()
+		if amount > cur_amount {
+			a.entries[key] = '${amount}'
+		}
+	} $else {
+		t := T{}
+		panic('${@STRUCT}.${@FN}: ${typeof(t).name} is not supported')
+	}
+}
+
+[if shy_analyse ?]
+pub fn min[T](key string, amount T) {
+	mut a := unsafe { analyse.hack } // TODO
+	$if T is int {
+		cur_amount := a.entries[key].int()
+		if amount < cur_amount {
+			a.entries[key] = '${amount}'
+		}
+	} $else $if T is u32 {
+		cur_amount := a.entries[key].u32()
+		if amount < cur_amount {
+			a.entries[key] = '${amount}'
+		}
+	} $else $if T is i64 {
+		cur_amount := a.entries[key].i64()
+		if amount < cur_amount {
+			a.entries[key] = '${amount}'
+		}
+	} $else $if T is u64 {
+		cur_amount := a.entries[key].u64()
+		if amount < cur_amount {
+			a.entries[key] = '${amount}'
+		}
+	} $else $if T is f32 {
+		cur_amount := a.entries[key].f32()
+		if amount < cur_amount {
+			a.entries[key] = '${amount}'
+		}
+	} $else $if T is f64 {
+		cur_amount := a.entries[key].f64()
+		if amount < cur_amount {
+			a.entries[key] = '${amount}'
+		}
 	} $else {
 		t := T{}
 		panic('${@STRUCT}.${@FN}: ${typeof(t).name} is not supported')
