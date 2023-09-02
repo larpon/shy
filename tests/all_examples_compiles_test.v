89d13b108b25037d8498ecea278a8f4cf663e49b
lmp <dev.larpon@gmail.com>
2023-09-02 20:10:16 +0200

    tests: add test that ensures all examples can compile

---
+++ tests/all_examples_compiles_test.v
@@ -0,0 +1,12 @@
+// Copyright(C) 2023 Lars Pontoppidan. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
+import os
+
+const vexe = os.real_path(os.getenv_opt('VEXE') or { @VEXE })
+
+const examples_dir = os.join_path(@VMODROOT, 'examples')
+
+fn test_all_examples_compiles() {
+	os.execute_or_panic('${vexe} should-compile-all -c "${examples_dir}"')
+}
