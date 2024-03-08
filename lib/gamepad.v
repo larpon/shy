a23363cfd620e3254ba0746b32d8d9afd6e7b8b1
lmp <dev.larpon@gmail.com>
2024-03-08 18:56:10 +0100

    gamepad: test building on SDL 2.0.14

--- lib/gamepad.v
+++ lib/gamepad.v
@@ -83,10 +83,9 @@ pub fn GamepadButton.from_sdl_controller_button(button sdl.GameControllerButton)
 		.dpad_right {
 			GamepadButton.dpad_right
 		}
-		// TODO this does not work with SDL 2.0.8
-		//.misc1 {
-		//	GamepadButton.misc_1
-		//}
+		.misc1 {
+			GamepadButton.misc_1
+		}
 		.paddle1 {
 			GamepadButton.paddle_1
 		}
